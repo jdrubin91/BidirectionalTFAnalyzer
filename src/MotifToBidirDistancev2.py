09d2bfec434cf92de9645b78a827b49b16f41da6
jdrubin91 <jd.rubin91@gmail.com>
2015-10-06 10:35:12 -0600

    Added MotifToBidirDistancev2 which gives bidirectional sites a 1kb pad when searching for corresponding motif sites and then length normalizes all distances

---
+++ src/MotifToBidirDistancev2.py
@@ -0,0 +1,54 @@
+__author__ = 'Jonathan Rubin'
+
+import os
+import Functions
+import math
+
+def run(directory, bidirectionalfilepath, homedir):
+    directorylist = Functions.fimo_directories(directory)
+    
+        
+            
+                
+
+
+if __name__ == "__main__":
+    #Runs on original HCT116 directory
+    
+    def fimo_directories(rootdirectory):
+        directorylist = []
+        for TF in os.listdir(rootdirectory):
+            if os.path.exists(rootdirectory + "/" + TF + "/peak_files/MEME"):
+                os.chdir(rootdirectory + "/" + TF + "/peak_files/MEME")
+                FileList = [item for item in os.listdir(os.getcwd()) if 'fimo_out' in item]
+                for fimofolder in FileList:
+                    directorylist.append(rootdirectory + "/" + TF + "/peak_files/MEME/" + fimofolder)
+        
+        return directorylist
+    
+    directory = "/projects/dowellLab/ENCODE/HCT116"
+    bidirectionalfilepath = "/Users/joru1876/ENCODEBidirectional/bidirectional_hits.merge.bed"
+    homedir = "/Users/joru1876/BidirectionalTFAnalyzer/src"
+    directorylist = fimo_directories(directory)
+    distances = dict()
+    for item in directorylist:
+        os.chdir(item)
+        TF = item.split('/')[5]
+        Motif = item.split('/')[8]
+        distances[TF] = [Motif, Functions.get_distances_pad(bidirectionalfilepath, False, "fimo.rmdup.ord.cut.merge.bed", True, 1000)]
+                
+    os.chdir(homedir)
+    os.chdir('..')
+    os.chdir("files")
+    outfile = open("TFMotifToBidirDistancePad.txt",'w')
+    for key in distances:
+        outfile.write(key)
+        outfile.write("\n")
+        for item in distances[key]: 
+            if 'fimo_out' in item:   
+                outfile.write(str(item))
+            else:
+                for value in item:
+                    outfile.write(str(value))
+                    outfile.write(",")
+            outfile.write("\n")
