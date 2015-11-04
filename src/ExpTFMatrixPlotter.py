f1823a29bf6785f13fcad54615649e6935509c00
jdrubin91 <jd.rubin91@gmail.com>
2015-11-04 15:32:16 -0700

    updates

--- src/ExpTFMatrixPlotter.py
+++ src/ExpTFMatrixPlotter.py
@@ -2,3 +2,34 @@ __author__ = 'Jonathan Rubin'
 
 import matplotlib
 
+def run(masterfile,pvalcutoff):
+    #Takes as input a master file with format 'Exp\tTF\tpval\tTF\tpval...etc'
+    #Returns three ordered lists ([exp],[TF],[TF vals])
+    masterdict = dict()
+    for line in open(masterfile):
+        linetabsplit = line.strip().split()
+        exp = linetabsplit[0][0,linetabsplit[0].index('.')]
+        masterdict[exp] = list()
+        linecommasplit = linetabsplit[1].split(',')
+        for i in range(0,len(linecommasplit)):
+            TF = linecommasplit[i]
+            i+=1
+            pval = linecommasplit[i]
+            if pval < pvalcutoff:
+                pval = 1
+            else:
+                pval = 0
+            masterdict[exp].append((TF,pval))
+            
+    
+    return masterdict
+    
+if __name__ == "__main__":
+    #Specify location of master file. Must be in format: 'Exp\tTF\tpval\tTF\tpval...etc'
+    masterfile = '/scratch/Users/joru1876/ExpTFMatrixMasterFile.txt'
+    #Specify pvalue cutoff to determine whether TF is 'active'
+    pvalcutoff = 0.0001
+    
+    masterdict = run(masterfile,pvalcutoff)
+    
+    print masterdict
