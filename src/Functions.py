7a0097a3a0914ba5c28b9a6e08e6eb0c080ebccc
jdrubin91 <jd.rubin91@gmail.com>
2015-11-05 19:16:24 -0700

    updates

--- src/Functions.py
+++ src/Functions.py
@@ -324,8 +324,8 @@ def TFIT_fimo_directories(rootdirectory):
 def TFIT_EMG_OUT_directories(rootdirectory):
     directorylist = []
     for exp in os.listdir(rootdirectory):
-        if os.path.exists(rootdirectory + '/' + exp + '/EMG_out_files/'):
-            directorylist.append(rootdirectory + '/' + exp + '/EMG_out_files/')
+        if os.path.exists(rootdirectory + '/' + exp + '/EMG_out_files'):
+            directorylist.append(rootdirectory + '/' + exp + '/EMG_out_files')
     
     return directorylist
     
