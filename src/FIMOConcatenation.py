c0799fb59a0f4c8cfc2e07836407d08bc6bf350c
jdrubin91 <jd.rubin91@gmail.com>
2015-11-13 17:27:45 -0700

    updates

--- src/FIMOConcatenation.py
+++ src/FIMOConcatenation.py
@@ -7,16 +7,16 @@ def run(fimodir):
     
     fimofile = fimodir + '/fimo.txt'
     
-    Functions.cut_file(fimofile, [1,2,3], fimodir + '/fimo.cut.txt')
     
-    
-    return Functions.parse_file(fimodir + '/fimo.cut.txt')[1:len(Functions.parse_file(fimodir + '/fimo.cut.txt'))]
+    return Functions.parse_file(fimodir + '/fimo.txt')[1:len(Functions.parse_file(fimodir + '/fimo.txt'))]
     
 if __name__ == "__main__":
     directory = '/scratch/Shares/dowell/ENCODE/HCT116v2'
     
     for TF in os.listdir(directory):
+        print TF
         if os.path.exists(directory + '/' + TF + '/peak_files/outfiles/MEME'):
+            print 'exists'
             for fimofolder in [i for i in os.listdir(directory + '/' + TF + '/peak_files/outfiles/MEME') if 'fimo_out' in i and i[0].isdigit()]:
                 fimodir = directory + '/' + TF + '/peak_files/outfiles/MEME/' + fimofolder
                 fimodict = dict()
