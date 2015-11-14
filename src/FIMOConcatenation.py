22aa0b9729dd756c2166a2f5b8e3fb19fe4b6bfe
jdrubin91 <jd.rubin91@gmail.com>
2015-11-13 17:17:52 -0700

    updates

--- src/FIMOConcatenation.py
+++ src/FIMOConcatenation.py
@@ -16,15 +16,16 @@ if __name__ == "__main__":
     directory = '/scratch/Shares/dowell/ENCODE/HCT116v2'
     
     for TF in os.listdir(directory):
-        for fimofolder in [i for i in os.listdir(directory + '/' + TF + '/peak_files/outfiles/MEME') if 'fimo_out' in i and i[0].isdigit()]:
-            fimodir = directory + '/' + TF + '/peak_files/outfiles/MEME/' + fimofolder
-            fimodict = dict()
-            fimodict[fimofolder] = run(fimodir)
-        outfile = open(directory + '/' + TF + '/peak_files/outfiles/MEME/fimo.cat.txt','w')
-        for key in fimodict:
-            for item in fimodict[key]:
-                for val in item:
-                    outfile.write(val)
-                    outfile.write('\t')
-                outfile.write(key)
-                outfile.write('\n')
+        if os.path.exists(directory + '/' + TF + '/peak_files/outfiles/MEME'):
+            for fimofolder in [i for i in os.listdir(directory + '/' + TF + '/peak_files/outfiles/MEME') if 'fimo_out' in i and i[0].isdigit()]:
+                fimodir = directory + '/' + TF + '/peak_files/outfiles/MEME/' + fimofolder
+                fimodict = dict()
+                fimodict[fimofolder] = run(fimodir)
+            outfile = open(directory + '/' + TF + '/peak_files/outfiles/MEME/fimo.cat.txt','w')
+            for key in fimodict:
+                for item in fimodict[key]:
+                    for val in item:
+                        outfile.write(val)
+                        outfile.write('\t')
+                    outfile.write(key)
+                    outfile.write('\n')
