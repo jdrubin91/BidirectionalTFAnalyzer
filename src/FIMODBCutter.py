e1d8f5b306241ac7ff224a44eaf88054ac09b007
jdrubin91 <jd.rubin91@gmail.com>
2016-01-20 14:57:21 -0700

    updates

--- src/FIMODBCutter.py
+++ src/FIMODBCutter.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
         os.chdir(filepath + '/' + TF + '/')
         file1 = 'fimo.txt'
         if os.path.exists(filepath + '/' + TF + '/fimo.txt'):
+            print TF
             Functions.cut_file(file1, [1,2,3,4,5,6,7,8], Functions.get_mod_filename(file1,'cut'))
     
